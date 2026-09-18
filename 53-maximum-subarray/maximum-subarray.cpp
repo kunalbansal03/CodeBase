c216736778b3c00d445b10209564eaafd6622a29
kunalbansal03 <134192272+kunalbansal03@users.noreply.github.com>
2026-09-18 14:40:03 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.10%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int sum = 0;
         int maxi = INT_MIN;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){ 
             sum += nums[i];
             if(sum > maxi) maxi = sum;
             if(sum < 0) sum = 0;
